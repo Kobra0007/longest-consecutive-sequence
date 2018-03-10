61e10043bb8fc213728e92b33f8bcb0dc717cd5e
Kobra0007 <kobra007@github.com>
2018-03-10 23:20:20 +0300

    80%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+    var count = 1,
+        arrCount = [];
+
+    function compare(a, b) {
+        return a - b;
+    }
+    array.sort(compare);
+
+    array.map((item, index, arr) => {
+
+        if (arr[index + 1] === item + 1) {
+            count++;
+        } else if (count > 1) {
+            arrCount.push(count);
+            count = 1;
+        }
+
+    });
+
+
+    arrCount.sort(compare);
+
+    return arrCount[arrCount.length - 1];
 }
